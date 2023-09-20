c72737cb69140bbfb29e5f0526c4572c5484744e
YaninaAlvarez <yanialva2009@hotmail.com>
2023-09-20 17:38:51 -0300

    modificacion de eq, set y get. Test de celda. Ref #17 #18

--- test/test_board.py
+++ test/test_board.py
@@ -2,7 +2,7 @@ import pytest
 
 from models.logic.board import Board
 
-from models.logic.celda import Celda
+from models.logic.cell import Cell
 
 @pytest.fixture
 def board():
@@ -10,21 +10,21 @@ def board():
 
 
 def test_initial_board(board):
-     assert board.rows == 2
-     assert board.columns == 2
-     assert board.get_celda(0, 0).__eq__(Celda())
-     assert board.get_celda(0, 1).__eq__(Celda())
-     assert board.get_celda(1, 0).__eq__(Celda())
-     assert board.get_celda(1, 1).__eq__(Celda())
+     assert board._rows == 2
+     assert board._columns == 2
+     assert board.get_cell(0, 0).is_empty()
+     assert board.get_cell(0, 1).is_empty()
+     assert board.get_cell(1, 0).is_empty()
+     assert board.get_cell(1, 1).is_empty()
 
 def test_initial_spwn(board):
-     assert board.rows == 2
-     assert board.columns == 2
+     assert board._rows == 2
+     assert board._columns == 2
      board.set_position_spawn_bacterium(0,0)
      board.set_position_spawn_other(1,1) 
-     assert board.get_celda(0, 0)._spawn_bacterium().__eq__(True)
-     assert board.get_celda(0, 1).__eq__(Celda())
-     assert board.get_celda(1, 0).__eq__(Celda())
-     assert board.get_celda(1, 1)._spawn_other().__eq__(True)
+     assert board.get_cell(0, 0).get_spawn_bacterium() == True
+     assert board.get_cell(0, 1).is_empty()
+     assert board.get_cell(1, 0).is_empty()
+     assert board.get_cell(1, 1).get_spawn_other() == True                                                                                                                                                                                                                                
 
 
