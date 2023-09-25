5c439ab50dd1a85536332a5178ddecdab1ab2a52
santiago <santiagovazquez010@gmail.com>
2023-09-25 19:43:56 -0300

    agregado del str y test en board #17 #18

--- models/logic/cell.py
+++ models/logic/cell.py
@@ -1,5 +1,9 @@
+from models.logic.Bacterium import *
+from models.logic.Antibiotic import *
+from models.logic.Bacteriophage import *
 
 class Cell:
+
 
 	def __init__(self):
 		self.__bacteria = []
@@ -7,6 +11,36 @@ class Cell:
 		self.__bacteriophages = []
 		self.__spawn_bacterium = False
 		self.__spawn_other = False
+
+	def __str__(self):
+			res = ' '
+			for _ in range(0,self.__antibiotics):
+				 
+				res = self.__antibiotics.__str__()+ res
+
+			for bac in self.__bacteria:
+				res = bac.__str__() + res
+
+			for vir in self.__bacteriophages:
+			 	res = vir.__str__() + res
+			
+			return res
+			
+	@staticmethod
+	def from_string(cell_str):
+			if cell_str == BacteriumNormal(0).__str__():
+				return BacteriumNormal(0)
+			elif cell_str == BacteriumStrong(0).__str__():
+				return BacteriumStrong(0)
+			elif cell_str == BacteriumInfected(0).__str__():
+				return BacteriumInfected(0)
+			elif cell_str == BacteriumWeak(0).__str__():
+				return BacteriumWeak(0)
+			elif (cell_str == Antibiotic().__str__()):
+				return Antibiotic()
+			else:
+				raise ValueError(f'Invalid cell string: {cell_str}')
+	
 
 	def get_spawn_other(self):
 		return self.__spawn_other
@@ -27,22 +61,36 @@ class Cell:
 		else:
 			raise ValueError(f'celda ocupada')
 
+	#def __eq__(self, other):
+	#	return isinstance(other, Cell)
+	def __eq__(self, other):
+		if self._antibiotics == other._antibiotics and self._bacteria.__eq__(other._bacteria) and self._bacteriophages.__eq__(other._bacteriophages) and self.__spawn_bacterium.__eq__(other.__spawn_bacterium) and self.__spawn_other.__eq__(other.__spawn_other):
+			return True
+		return False
+	
+	def is_empty(self):
+		if self._antibiotics == 0 and self._cant_bacteria() == 0 and self._cant_bacteriophages() == 0 and not(self.__spawn_bacterium or self.__spawn_other):
+			return True
+		return False
+
 	@property
 	def _bacteria(self):
 		return self.__bacteria
 	
 	@_bacteria.setter
-	def _bacteria(self, state, moves ):
+	def _bacteria(self, bacteria:Bacterium):
 		#crear bacteria del tipo correcto
-		self.__bacteria.append()
+		self.__bacteria.append(bacteria)
 
-	@_bacteria.setter
-	def _bacteria(self, state ):
-	#crear bacteria del tipo correcto
-		self.__bacteria.append()
-
+	#def set_bacterias(self, state:Type_Bacterium):
+	#	self.bacterias.append([Bacterium(state)])
+	
 	def _cant_bacteria(self):
 		return self.__bacteria.__len__()
+	
+	#def set_bacterias(self, bacter:Bacterium, moves:int):
+	#	self.bacterias.append
+
 	
 	@property
 	def _antibiotics(self):
---
+++ test/test_celda.py
@@ -0,0 +1,58 @@
+import pytest
+
+from models.logic.board import Board
+
+from models.logic.cell import Cell
+
+from models.logic.Bacterium import *
+
+@pytest.fixture
+def cell():
+    return Cell()
+
+
+def test_initial_cell(cell):
+    assert cell._antibiotics == 0
+    assert cell._cant_bacteria() == 0
+    assert cell._cant_bacteriophages() == 0
+    assert cell.get_spawn_bacterium() == False
+    assert cell.get_spawn_other() == False
+    assert cell.is_empty()
+    
+
+def test_spawn_bacterium_cell(cell):
+    cell.set_spawn_bacterium()
+    assert cell._antibiotics == 0
+    assert cell._cant_bacteria() == 0
+    assert cell._cant_bacteriophages() == 0
+    assert cell.get_spawn_bacterium() == True
+    assert cell.get_spawn_other() == False
+
+def test_spawn_other_cell(cell):
+    cell_aux = Cell()
+    cell_aux.set_spawn_other()
+    cell.set_spawn_other()
+    assert cell._antibiotics == 0
+    assert cell._cant_bacteria ()== 0
+    assert cell._cant_bacteriophages() == 0
+    assert cell.get_spawn_bacterium() == False
+    assert cell.get_spawn_other() == True
+
+def test_spawn_bacterium_cell_not_empty(cell):
+    cell.set_spawn_other()
+    with pytest.raises(ValueError):
+        cell.set_spawn_bacterium()
+
+def test_spawn_other_cell_not_empty(cell):
+    cell.set_spawn_bacterium()
+    with pytest.raises(ValueError):
+        cell.set_spawn_other()
+
+def test_eq_cell(cell):
+    cell_aux = Cell()
+    cell_aux.set_spawn_other()
+    cell.set_spawn_other()
+    assert cell.__eq__(cell_aux)
+
+
+
