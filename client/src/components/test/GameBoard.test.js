66fdd2613a8f6de0d5ffaf742772f34e2716984c
Kevin <kevinriberi@gmail.com>
2023-11-26 12:52:47 -0300

    Se acomodo el test de React de Game_Board de acuerdo al cambio de nombre en los archivos

--- client/src/components/test/GameBoard.test.js
+++ client/src/components/test/GameBoard.test.js
@@ -1,12 +1,12 @@
 import { render, screen, fireEvent } from '@testing-library/react'
-import Create_board from '../board';
+import Create_Game from '../GameBoard';
 
-describe("<Create_board />", () =>{
+describe("<Create_Game />", () =>{
 
   let table;
   let BarraVelocidad;
   beforeEach(() =>{
-    render(<Create_board handleStartGame={true} />);
+    render(<Create_Game handleStartGame={true} />);
     BarraVelocidad = screen.getByText(/Velocidad:/i);
     table = screen.getByRole('table');
   });
