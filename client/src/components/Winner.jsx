a3d0278c5fbe071eb07e324f45744cf09186bfc3
Kevin <kevinriberi@gmail.com>
2023-12-02 12:31:08 -0300

    Se agrego efecto de fade-in cuando aparece el cartel correspondiente a fin de juego

--- client/src/components/Winner.jsx
+++ client/src/components/Winner.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../css/Winner.css'; 
 
 const Winner = ({ winnerType }) => {
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    // Espera un breve momento antes de cambiar la opacidad para permitir el efecto de fade-in
+    const timeoutId = setTimeout(() => {
+      setVisible(true);
+    }, 100);
+
+    // Limpia el temporizador al desmontar el componente
+    return () => clearTimeout(timeoutId);
+  }, []); // El segundo argumento vacío significa que se ejecutará solo en la montura del componente
+
   const getImageSource = () => {
     switch (winnerType) {
       case 'Game_Winner.BACTERIUM':
@@ -14,8 +26,12 @@ const Winner = ({ winnerType }) => {
   };
 
   return (
-    <img className={`winning-image show`} src={getImageSource()} />
-    );
+    <img
+      className={`winning-image ${visible ? 'show' : ''}`}
+      src={getImageSource()}
+      alt="Winner"
+    />
+  );
 };
 
 export default Winner;
